0a446312a2c568c1d6b4f6b1e44236a92b5a4987
Miracle0409 <141814436+Miracle0409@users.noreply.github.com>
2023-08-23 01:46:06 +0700

    Update short.php

--- short.php
+++ short.php
@@ -19,17 +19,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>URL Shortener - Shorten URL</title>
-</head>
-<body>
-    <h1>Shorten URL</h1>
-    <form action="shorten.php" method="post">
-        <input type="text" name="long_url" placeholder="Long URL" required><br>
-        <input type="text" name="slug" placeholder="Slug" required><br>
-        <button type="submit">Shorten</button>
-    </form>
-</body>
-</html>
