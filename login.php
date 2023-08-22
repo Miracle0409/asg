aa1e67b4d2b253460782a7091b6c7292e7b4ba59
Miracle0409 <141814436+Miracle0409@users.noreply.github.com>
2023-08-23 01:36:26 +0700

    Update login.php

--- login.php
+++ login.php
@@ -2,12 +2,17 @@
 <html>
 <head>
     <title>URL Shortener - Login</title>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 <body>
     <h1>Login</h1>
     <form action="login_process.php" method="post">
         <input type="text" name="username" placeholder="Username" required><br>
         <input type="password" name="password" placeholder="Password" required><br>
+
+        <!-- reCAPTCHA widget -->
+        <div class="g-recaptcha" data-sitekey="YOUR_PUBLIC_KEY"></div>
+
         <button type="submit">Login</button>
     </form>
 </body>
